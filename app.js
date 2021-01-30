72ca3b8cd08bc8de3fe45ab2d3377fcf7859ade1
surabhiadak <surabhiadak8@gmail.com>
2021-01-30 19:06:11 +0530

    commit: style

---
+++ app.js
@@ -0,0 +1,27 @@
+function todoList()
+{
+    //13 is keycode for enter button on keyboard
+
+    const list = document.querySelector('.list');
+    const input = document.querySelector('input');
+    const newTask = document.createElement('li');
+    const span = document.createElement('span');
+
+    span.innerHTML = '<i class="fas fa-trash"></i>';
+
+    if(input.value !== ''){
+        newTask.textContent = input.value;
+        input.value = '';   
+        list.appendChild(newTask); 
+        newTask.appendChild(span);       
+    }
+
+    span.addEventListener('click', function(){
+        const parent = this.parentNode;
+        parent.remove();
+    });
+
+    newTask.addEventListener('click', ()=>{
+        newTask.classList.toggle('completed');
+    });
+}
